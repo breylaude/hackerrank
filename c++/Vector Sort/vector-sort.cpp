35d3012208323546472a457cb2a2c2ec871ffa00
Ignas Laude <laude822@gmail.com>
2022-09-18 11:51:50 -0700

    Create vector-sort.cpp

---
+++ c++/Vector Sort/vector-sort.cpp
@@ -0,0 +1,21 @@
+/* Author: Ignas Laude
+ * Date: 09/19/22 3:52 AM
+*/
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+     int n; cin >> n;
+     vector <int> arr;
+     int x;
+     for (int i=0; i<n; i++)
+         {
+        cin >> x;
+        arr.push_back(x);
+    }
+   sort(arr.begin(),arr.end());
+    for(int i=0 ; i<arr.size();i++)
+    {
+        cout<< arr[i] <<' ';
+    }
+    return 0;
+}
