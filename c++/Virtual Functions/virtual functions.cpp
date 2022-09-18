5012f7695b200fd0e13e38606d467ac720666f14
Ignas Laude <laude822@gmail.com>
2022-09-18 06:12:39 -0700

    Create virtual functions.cpp

---
+++ c++/Virtual Functions/virtual functions.cpp
@@ -0,0 +1,68 @@
+/* Author: Ignas Laude
+ * Date : September 18, 2022
+*/
+
+/* IMPORTANT:
+ * This code is meant to be used as a solution on HackerRank (link above).
+ * It is not meant to be executed as a standalone program.
+ */
+
+
+// Include the string header to simplify input/output of strings
+class Person {
+public:
+    string name;
+    int age;
+    virtual void getdata() {
+        cin >> this->name >> this->age;
+    }
+    virtual void putdata() {
+        cout << this->name << " " << this->age << endl;
+    }
+};
+class Professor : public Person {
+public:
+    Professor() {
+        this->cur_id = ++id;
+    }
+    int publications;
+    static int id;
+    int cur_id;
+    void getdata() {
+        cin >> this->name >> this->age >> this->publications;
+    }
+    void putdata() {
+        cout << this->name << " "
+            << this->age << " "
+            << this->publications << " "
+            << this->cur_id << endl;
+    }
+};
+int Professor::id = 0;
+class Student : public Person {
+#define NUM_OF_MARKS 6
+public:
+    Student() {
+        this->cur_id = ++id;
+    }
+    int marks[NUM_OF_MARKS];
+    static int id;
+    int cur_id;
+    void getdata() {
+        cin >> this->name >> this->age;
+        for (int i=0; i<NUM_OF_MARKS; i++) {
+            cin >> marks[i];
+        }
+    }
+    void putdata() {
+        int marksSum = 0;
+        for (int i=0; i<NUM_OF_MARKS; i++) {
+            marksSum += marks[i];
+        }
+        cout << this->name << " "
+            << this->age << " "
+            << marksSum << " "
+            << this->cur_id << endl;
+    }
+};
+int Student::id = 0;
